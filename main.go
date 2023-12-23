49bb26236e9c82ee7ba90056ecf07813c028377f
Vishal21121 <vs84077000@gmail.com>
2023-12-23 19:48:46 +0530

    modified: file strcuture

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
 	app := fiber.New()
+
 	app.Get("/", func(c *fiber.Ctx) error {
 		c.SendStatus(200)
 		return c.JSON(fiber.Map{
@@ -15,5 +20,6 @@ func main() {
 		})
 	})
 
-	app.Listen(":8080")
+	log.Printf("\nServer is listening on port %s", ":8080")
+	log.Fatal(app.Listen(":8080"))
 }
--- model/user.model.go
+++ model/user.model.go
@@ -4,7 +4,7 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-type User struct {
+type UserRegister struct {
 	ID           primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
 	Username     string             `json:"username"`
 	Email        string             `json:"email"`
